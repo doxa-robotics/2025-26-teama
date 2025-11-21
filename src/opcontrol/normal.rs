f370ced137695f7622a1abaaf7009456bca0570b
zabackary <137591653+zabackary@users.noreply.github.com>
2025-11-21 14:44:30 +0900

    refactor: update controls to run third motor when either stage runs

--- src/opcontrol/normal.rs
+++ src/opcontrol/normal.rs
@@ -55,24 +55,43 @@ pub async fn opcontrol(robot: &mut Robot) -> Result<!, OpcontrolError> {
             units: libdoxa::subsystems::drivetrain::drivetrain_pair::DrivetrainUnits::Voltage,
         });
 
-        if state.button_r1.is_now_pressed() {
-            robot.intake.activate_front_intake().context(IntakeSnafu)?;
-        }
-        if state.button_r2.is_now_pressed() {
-            robot.intake.reverse_front_intake().context(IntakeSnafu)?;
-        }
-        if state.button_r1.is_now_released() || state.button_r2.is_now_released() {
-            robot.intake.brake().context(IntakeSnafu)?;
+        // (r1, r2, l1, l2)
+        if state.button_r1.is_pressed()
+            || state.button_r2.is_pressed()
+            || state.button_l1.is_pressed()
+            || state.button_l2.is_pressed()
+        {
+            // If the left triggers are pressed, it controls the lift.
+            // If the right triggers are pressed, the intake works, but the lift
+            // lower motor should still spin.
+
+            if state.button_l2.is_pressed() {
+                // High lift
+                robot.lift.lift_to_high().context(LiftSnafu)?;
+            } else if state.button_l1.is_pressed() {
+                // Low lift
+                robot.lift.lift_to_medium().context(LiftSnafu)?;
+            } else {
+                // Follow the right triggers, which dictate the intake.
+                robot
+                    .lift
+                    .intake(if state.button_r2.is_pressed() {
+                        Direction::Forward
+                    } else {
+                        Direction::Reverse
+                    })
+                    .context(LiftSnafu)?;
+            }
+        } else {
+            robot.lift.brake().context(LiftSnafu)?;
         }
 
-        if state.button_l1.is_now_pressed() {
-            robot.lift.lift_to_high().context(LiftSnafu)?;
-        }
-        if state.button_l2.is_now_pressed() {
-            robot.lift.lift_to_medium().context(LiftSnafu)?;
-        }
-        if state.button_l1.is_now_released() || state.button_l2.is_now_released() {
-            robot.lift.brake().context(LiftSnafu)?;
+        if state.button_r2.is_pressed() {
+            robot.intake.activate_front_intake().context(IntakeSnafu)?;
+        } else if state.button_r1.is_pressed() {
+            robot.intake.reverse_front_intake().context(IntakeSnafu)?;
+        } else {
+            robot.intake.brake().context(IntakeSnafu)?;
         }
 
         // y is match load
