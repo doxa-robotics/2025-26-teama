ce230fd10df2017280f3abbe125fea14a73b049d
zabackary <137591653+zabackary@users.noreply.github.com>
2026-01-19 13:57:53 +0900

    chore: remove skills_safe route

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -4,7 +4,6 @@ mod left_primary;
 mod none;
 mod right_primary;
 mod skills;
-mod skills_safe;
 mod test;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
@@ -28,7 +27,7 @@ impl std::fmt::Display for Category {
 /// when testing with the legacy competition switch.
 pub const TESTING_ROUTE: Option<usize> = Some(2);
 
-pub const ROUTES: [doxa_selector::Route<Category, super::Robot>; 8] = [
+pub const ROUTES: [doxa_selector::Route<Category, super::Robot>; 7] = [
     left_primary::ROUTE,
     left_long_first::ROUTE,
     left_new::ROUTE,
@@ -37,5 +36,4 @@ pub const ROUTES: [doxa_selector::Route<Category, super::Robot>; 8] = [
     none::ROUTE,
     test::ROUTE,
     skills::ROUTE,
-    skills_safe::ROUTE,
 ];
