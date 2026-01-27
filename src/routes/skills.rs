f230816650584b96c5113801f2a40626403c243b
zabackary <137591653+zabackary@users.noreply.github.com>
2026-01-27 21:08:46 +0900

    feat: improve skills

--- src/routes/skills.rs
+++ src/routes/skills.rs
@@ -21,21 +21,41 @@ pub const ROUTE: doxa_selector::Route<super::Category, crate::Robot> = doxa_sele
     route
 );
 
+async fn match_load(robot: &mut crate::Robot) {
+    robot.drivetrain.action(VoltageAction {
+        voltage: DrivetrainPair::new_voltage(10.0, 10.0),
+    }); // Intentionally not awaited
+    robot.intake.intake();
+    sleep(Duration::from_millis(1500)).await;
+    robot.intake.brake();
+    robot.drivetrain.action(VoltageAction {
+        voltage: DrivetrainPair::from(0.0),
+    }); // Intentionally not awaited
+    sleep(Duration::from_millis(250)).await;
+    robot.drivetrain.action(VoltageAction {
+        voltage: DrivetrainPair::from(0.0),
+    }); // Intentionally not awaited
+    robot.intake.intake();
+    sleep(Duration::from_millis(1250)).await;
+    robot.intake.brake();
+}
+
 async fn route(robot: &mut crate::Robot) -> () {
     log::info!("Route: skills");
     robot
         .tracking
         .set_current(Point2::new(-600.0 + 170.0, -1375.0), Angle::HALF_TURN);
+
+    // MARK: match load 1
     // Head to the match loader first
     robot.drivetrain.action(forward(100.0, CONFIG)).await;
     robot
         .drivetrain
         .action(drive_to_point(
-            Point2::new(-2.0.tiles(), -2.0.tiles()),
+            Point2::new(-1.95.tiles(), -2.0.tiles()),
             CONFIG,
         ))
         .await;
-    return;
     robot.intake.intake();
     robot.match_loader.extend();
     robot
@@ -47,29 +67,83 @@ async fn route(robot: &mut crate::Robot) -> () {
             ),
         )
         .await;
-    // Hold the position while loading
-    robot.drivetrain.action(VoltageAction {
-        voltage: DrivetrainPair::new_voltage(10.0, 10.0),
-    }); // Intentionally not awaited
-    robot.intake.intake();
-    sleep(Duration::from_millis(1500)).await;
+    // Load with the macro
+    match_load(robot).await;
+    robot
+        .drivetrain
+        .action(forward(-200.0, CONFIG.with_linear_error_tolerance(60.0)))
+        .await;
 
+    // MARK: goal 1
     // Drive to other side of goal
     robot
         .drivetrain
-        .action(
-            PurePursuitAction::new(
-                CubicParametricPath::new(
-                    Point2::new(-1200.0, -1200.0),
-                    -Angle::QUARTER_TURN,
-                    2882.0,
-                    Point2::new(-1200.0, 1200.0),
-                    Angle::from_radians(2.4085543677521746),
-                    4214.0,
-                ),
-                CONFIG,
-            )
-            .reversed(),
-        )
+        .action(drive_to_point(
+            Point2::new(-1.2.tiles(), -1.0.tiles()),
+            CONFIG.with_linear_error_tolerance(60.0),
+        ))
         .await;
+    robot
+        .drivetrain
+        .action(drive_to_point(
+            Point2::new(-1.2.tiles(), 1.0.tiles()),
+            CONFIG.with_linear_error_tolerance(60.0),
+        ))
+        .await;
+    robot
+        .drivetrain
+        .action(drive_to_point(
+            Point2::new(-2.0.tiles(), 2.0.tiles()),
+            CONFIG.with_linear_error_tolerance(60.0),
+        ))
+        .await;
+    robot.match_loader.retract();
+    robot
+        .drivetrain
+        .action(drive_to_point(Point2::new(-2.0.tiles(), 1.2.tiles()), CONFIG).reversed())
+        .await;
+
+    // Calibrate position against long goal
+    robot.tracking.set_current(
+        Point2::new(-2.0.tiles(), robot.tracking.current().offset.y),
+        robot.tracking.current().heading,
+    );
+
+    robot.intake.outtake_long_anti_jam().await;
+    sleep(Duration::from_millis(2750)).await;
+
+    // MARK: match load 2
+    // Head to the match loader
+    robot.match_loader.extend();
+    robot
+        .drivetrain
+        .action(drive_to_point(
+            Point2::new(-1.97.tiles(), 2.6.tiles()),
+            CONFIG.with_linear_error_tolerance(100.0),
+        ))
+        .await;
+    // Hold the position while loading
+    // Load with the macro
+    match_load(robot).await;
+    robot
+        .drivetrain
+        .action(forward(-200.0, CONFIG.with_linear_error_tolerance(60.0)))
+        .await;
+
+    // MARK: goal 2
+    // Drive backwards to goal
+    robot
+        .drivetrain
+        .action(drive_to_point(Point2::new(-2.0.tiles(), 1.2.tiles()), CONFIG).reversed())
+        .await;
+    // Calibrate position against long goal
+    robot.tracking.set_current(
+        Point2::new(-2.0, robot.tracking.current().offset.y),
+        robot.tracking.current().heading,
+    );
+    robot.match_loader.retract();
+
+    robot.intake.outtake_long_anti_jam().await;
+    sleep(Duration::from_millis(2750)).await;
+    robot.drivetrain.action(forward(-200.0, CONFIG)).await;
 }
