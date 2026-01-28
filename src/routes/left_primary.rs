3e91f6d79f4b5bc5b5d10597fd8c8d8a09e72e4d
zabackary <137591653+zabackary@users.noreply.github.com>
2026-01-28 17:17:01 +0900

    feat: finish left primary

--- src/routes/left_primary.rs
+++ src/routes/left_primary.rs
@@ -17,22 +17,36 @@ pub const ROUTE: doxa_selector::Route<super::Category, crate::Robot> = doxa_sele
 
 async fn route(robot: &mut crate::Robot) -> () {
     log::info!("Route: left primary");
-    robot
-        .tracking
-        .set_current(Point2::new(-400.0, -1250.0), Angle::from_radians(1.84));
+    robot.tracking.set_current(
+        Point2::new(-600.0 + 190.0, -1200.0),
+        Angle::from_radians(1.84),
+    );
     // Drive to the left set of balls while intaking
     robot.intake.intake();
+    let mut match_loader = robot.match_loader.clone();
     robot
         .drivetrain
         .action(drive_to_point(
-            Point2::new(-1.0.tiles(), -1.2.tiles()),
+            Point2::new(-1.0.tiles(), -0.8.tiles()),
             CONFIG,
         ))
+        .with_once_callback(
+            |tracking| tracking.offset.y > -1.2.tiles(),
+            move || {
+                match_loader.extend();
+            },
+        )
         .await;
     // Outtake balls into the center top goal
     robot
         .drivetrain
-        .action(drive_to_point(Point2::new(-0.6.tiles(), -0.6.tiles()), CONFIG).reversed())
+        .action(
+            drive_to_point(
+                Point2::new(-0.55.tiles(), -0.4.tiles()),
+                CONFIG.with_linear_error_tolerance(300.0),
+            )
+            .reversed(),
+        )
         .await;
     robot
         .drivetrain
@@ -43,15 +57,17 @@ async fn route(robot: &mut crate::Robot) -> () {
             ),
         )
         .await;
+    robot.intake.outtake_long_anti_jam().await;
+    sleep(Duration::from_millis(400)).await;
     robot.intake.outtake_top_middle();
-    sleep(Duration::from_millis(1300)).await;
+    sleep(Duration::from_millis(800)).await;
     // Go to the match loader
     robot.intake.intake();
     let mut match_loader = robot.match_loader.clone();
     robot
         .drivetrain
         .action(boomerang_to_point(
-            Point2::new(-1.8.tiles(), -2.5.tiles()),
+            Point2::new(-1.9.tiles(), -2.6.tiles()),
             -Angle::QUARTER_TURN,
             CONFIG.with_boomerang_lead(0.65),
         ))
@@ -69,7 +85,7 @@ async fn route(robot: &mut crate::Robot) -> () {
         .await;
     // Hold the position while loading
     robot.drivetrain.action(VoltageAction {
-        voltage: DrivetrainPair::new_voltage(10.0, 10.0),
+        voltage: DrivetrainPair::from(8.0),
     }); // Intentionally not awaited
     robot.intake.intake();
     sleep(Duration::from_millis(1200)).await;
@@ -81,7 +97,7 @@ async fn route(robot: &mut crate::Robot) -> () {
         .drivetrain
         .action(
             drive_to_point(
-                Point2::new(-2.0.tiles(), -1.35.tiles()),
+                Point2::new(-2.0.tiles(), -1.2.tiles()),
                 CONFIG.with_linear_error_tolerance(100.0),
             )
             .reversed(),
